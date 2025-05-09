87288571cad1927027d56372aff230ffc69be7f9
jhonatanmizu <natanjesuss20@gmail.com>
2025-05-08 21:20:52 -0300

    chore: setup path alias in unit tests

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,23 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@': './src',
+          '@shared': './src/shared',
+          '@screens': './src/screens',
+          '@utils': './src/utils',
+          '@assets': './src/assets',
+          '@services': './src/services',
+          '@hooks': './src/hooks',
+          '@navigation': './src/navigation',
+          '@config': './src/config',
+        },
+      },
+    ],
+  ],
 };
