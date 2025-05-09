ea72ac54c2f53b5e00efd1243c30bd5b5c73f623
jhonatanmizu <natanjesuss20@gmail.com>
2025-05-08 22:15:30 -0300

    :wrench: chore: setup path aliases in babel config

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import React, {useEffect} from 'react';
 import Config from 'react-native-config';
 // Components
 import {Text} from 'react-native';
-import {AppWrapper} from '@modules/shared/components';
+import {AppWrapper} from '@shared/components';
 
 export default function App() {
   const environment = Config.ENVIRONMENT || 'development';
