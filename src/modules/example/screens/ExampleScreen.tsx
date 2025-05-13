8cb426e785134258e657389f5ccfe2b20a44df4b
jhonatanmizu <natanjesuss20@gmail.com>
2025-05-13 11:55:48 -0300

    :sparkles: feat: create example screen

---
+++ src/modules/example/screens/ExampleScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Box, Text} from '@shared/components';
+
+const ExampleScreen = () => {
+  return (
+    <Box>
+      <Text>ExampleScreen</Text>
+    </Box>
+  );
+};
+
+export default ExampleScreen;
